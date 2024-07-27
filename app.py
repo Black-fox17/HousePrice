893812dac2796e514a0af59f5471e97233afb85e
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2024-07-27 11:17:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,14 +33,17 @@ if st.button("Predict Price"):
                 loc_int = 1
             elif location == "rural":
                 loc_int = 2
-            input_values = np.array([house_age, num_bedrooms, num_bathrooms, area, loc_int]).reshape(1,-1)
-            with open("prediction.pkl","rb") as file:
-                model = pickle.load(file)
 
-            predicted_price = model.predict(input_values)
-            predicted_price = predicted_price.tolist()[0]
-            # Display the predicted price
-            st.success(f"Predicted House Price: ${predicted_price:,.2f}")
+            if not all([house_age, num_bedrooms, num_bathrooms, area, loc_int]):
+                st.error("Please fill out all the fields.")
+            else:
+                input_values = np.array([house_age, num_bedrooms, num_bathrooms, area, loc_int]).reshape(1,-1)
+                with open("prediction.pkl","rb") as file:
+                    model = pickle.load(file)
+                predicted_price = model.predict(input_values)
+                predicted_price = predicted_price.tolist()[0]
+                # Display the predicted price
+                st.success(f"Predicted House Price: ${predicted_price:,.2f}")
 
         except ValueError:
             st.error("Please enter valid numbers for House Age, Number of Bedrooms, Number of Bathrooms, and Area.")
